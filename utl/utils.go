122290e5053a6b406c54375b2a9581607e08ac5f
Jan Humpál <Jan.Humpal@Xillio.com>
2023-12-08 13:02:35 +0100

    Solution for 2023-07

--- utl/utils.go
+++ utl/utils.go
@@ -70,3 +70,25 @@ func GetSurroundingOnly[T any](Ts []T, i int) []T {
 	}
 	return []T{Ts[i-1], Ts[i+1]}
 }
+
+// GCD
+// Greatest Common Divisor via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// LCM Least Common Multiple via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
